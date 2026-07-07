05ce35e79837a22ca458da969a43a397f83d6749
uwagnerleand <lcunhaleandro@gmail.com>
2026-07-07 10:36:15 -0300

    Resolver

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/porto-fc-manager-simulator">
+    <BrowserRouter basename="/gamerFut">
       <App />
     </BrowserRouter>
   </React.StrictMode>
